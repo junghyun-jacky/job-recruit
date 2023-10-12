1d5cf67df346e053bb7b1d14b7984970fa598a88
박정현 <ros28081@naver.com>
2023-10-12 22:43:57 +0900

    [feat] member, company 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO company (company_id, company_name)
+VALUES (10, '원티드'),
+       (11, 'companyA'),
+       (12, 'companyB'),
+       (13, 'companyC');
+
+INSERT INTO member (member_id, member_name)
+VALUES (10, '홍길동');
